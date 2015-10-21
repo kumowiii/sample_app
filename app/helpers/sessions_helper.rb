372513f362541b5040458f9e7c570c17db22f97b
kumoiminori <your.email@example.commin1o3m.s.g@gmail.com>
2015-10-22 03:22:15 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,8 +20,21 @@ module SessionsHelper
 		@current_user ||= User.find_by(remember_token: remember_token)		 # @current_userが未定義の場合にのみ@current_userインスタンス変数に記憶トークンを設定
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	def sign_out
 		self.current_user = nil
 		cookies.delete(:remember_token)
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url
 	end
 end
